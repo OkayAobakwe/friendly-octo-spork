af3861a7e3a6b2660166241b84627b1c6f0937ca
aobakwe sometimes <okayaobakwe@gmail.com>
2021-10-21 11:55:18 +0200

    chore(setup): setup the projects environment

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowsParens: "avoid",
+  singleQuote: true,
+  brackerSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none"
+}
+
+module.exports = optioons
